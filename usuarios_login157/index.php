67cd87e6e2ca3d929872302017b533ed80abf7c4
rickz1nho <mhferreira28@gmail.com>
2022-03-03 20:27:15 -0300

    Botao logout

--- usuarios_login157/index.php
+++ usuarios_login157/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
+    <title>Home</title>
 </head>
 <body>
 
@@ -27,5 +27,6 @@
     <a href="cadastro_usuario.php"> Caso não tenha uma conta - Cadastre-se</a>
     <br />
     <a href="logout.php"> Sair </a>
+
 </body>
 </html>
