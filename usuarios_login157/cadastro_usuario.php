11eb9f2f9910755b8498f90c26715067c88899fd
rickz1nho <mhferreira28@gmail.com>
2022-03-09 23:16:33 -0300

    Confirmação para deletar usuário

--- usuarios_login157/cadastro_usuario.php
+++ usuarios_login157/cadastro_usuario.php
@@ -17,6 +17,12 @@
             echo $_SESSION['msg'];
             $_SESSION['msg'] = null;
         }
+
+        require_once __DIR__."/config.php";
+        if(isset($_SESSION['usuario'])){
+        echo  "<script>alert('Você já está logado!');</script>";
+        header("location: {$base_path}/index.php?msg=talogadoja");
+        }
     
     ?>
 
