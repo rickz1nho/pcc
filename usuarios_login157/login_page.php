88921804f018b9572e1f6ca21c063d844e47650a
gabrield11 <gabrieldinizm03@gmail.com>
2022-03-10 00:30:44 -0300

    front login

--- usuarios_login157/login_page.php
+++ usuarios_login157/login_page.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css" />
+    <title>Login do Usuário</title>
 </head>
 <body>
 
@@ -19,24 +20,23 @@ require_once __DIR__."/config.php";
     
 
 ?>
+ <h2 class="titulo">Login</h2>
+   <div class="login-box">
+       <div class="user-box">
+        <form action="app/controllers/login_controller.php" method="POST">
 
-    <form action="app/controllers/login_controller.php" method="POST">
-    
-
-        <input placeholder="usuario" type="text" name="field_usuario">
-        
-        <br />
-
-        <input placeholder="senha" type="password" name="field_senha">
-        
-        <br />
-        <input type="submit">
-    
+        <input placeholder="Usuário" type="text" name="field_usuario">
+        <input placeholder="Senha" type="password" name="field_senha">
+        </div>
+        <input class="button" type="submit" value="Confirmar">
     </form>
+</div>
     <br>
     <br>
-
+ 
+    <div class="cadastro2">
     <a href="cadastro_usuario.php"> Caso não tenha uma conta - Cadastre-se</a>
+</div>
 
 
 
